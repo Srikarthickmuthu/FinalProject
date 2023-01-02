745855e111d4a969221127540fbaeeeb93ce0bc9
srikarthickmuthu <srikarthick.muthu@aspiresys.com>
2023-01-02 18:22:00 +0530

    push

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { UserHomeComponent } from './user-home/user-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserHomeComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
