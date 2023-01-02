5a9a4e8cf8610424ba58565a0a6f33136841c20e
srikarthickmuthu <srikarthick.muthu@aspiresys.com>
2023-01-02 14:29:53 +0530

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'loginSignUp',loadChildren:()=>import('./login/login.module').then(m=>m.LoginModule)}
+  {path:'home',loadChildren:()=>import("./home/home.module").then(m=>m.HomeModule)},
+  {path:'loginSignUp',loadChildren:()=>import('./login/login.module').then(m=>m.LoginModule)},
+  {path:'',redirectTo:'/home',pathMatch:'full'}
 ];
 
 @NgModule({
