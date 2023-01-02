19f6f6e153bbec6cdcf29fb27050d7da95341d0c
srikarthickmuthu <srikarthick.muthu@aspiresys.com>
2023-01-02 13:54:58 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
-import { SignUpComponent } from './login/sign-up/sign-up.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignUpComponent
   ],
   imports: [
     BrowserModule,
