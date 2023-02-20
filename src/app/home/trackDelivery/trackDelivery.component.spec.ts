1c0fdb9d3069e32413b6e972bebadbd2a3ed33ba
srikarthickmuthu <srikarthick.muthu@aspiresys.com>
2023-02-20 17:59:09 +0530

    push

--- src/app/home/trackDelivery/trackDelivery.component.spec.ts
+++ src/app/home/trackDelivery/trackDelivery.component.spec.ts
@@ -4,6 +4,10 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { TrackDeliveryComponent } from './trackDelivery.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrService } from 'ngx-toastr';
 
 describe('TrackDeliveryComponent', () => {
   let component: TrackDeliveryComponent;
@@ -11,7 +15,11 @@ describe('TrackDeliveryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TrackDeliveryComponent ]
+      declarations: [ TrackDeliveryComponent ],
+      providers:[HttpClient,HttpHandler,{
+        provide:ToastrService, useValue:ToastrService
+      }],
+      imports:[RouterModule,RouterTestingModule],
     })
     .compileComponents();
   }));
